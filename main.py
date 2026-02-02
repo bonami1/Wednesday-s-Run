95c6e057715f1579b1f63997ce6661799fedf694
Nirmala Bonami <90679647+bonami1@users.noreply.github.com>
2026-02-02 14:17:59 +0100

    Add files via upload
    Création de la fenêtre

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((25, 25, 30))
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
